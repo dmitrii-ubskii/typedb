921b73dab49cad3a3db12a6e1d0bb1cc1cb6b9d0
cxdorn <christoph@typedb.com>
2024-11-11 13:01:07 +0000

    unpub type_stack in expression compiler

--- compiler/annotation/expression/expression_compiler.rs
+++ compiler/annotation/expression/expression_compiler.rs
@@ -43,7 +43,7 @@ pub struct ExpressionCompilationContext<'this> {
     expression_tree: &'this ExpressionTree<Variable>,
     variable_value_categories: &'this HashMap<Variable, ExpressionValueType>,
     parameters: &'this ParameterRegistry,
-    pub type_stack: Vec<ExpressionValueType>,
+    type_stack: Vec<ExpressionValueType>,
 
     instructions: Vec<ExpressionOpCode>,
     variable_stack: Vec<Variable>,
