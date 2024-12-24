6fac5bbf06ede1645a46cbfaa964fa18f8652b9b
Krishnan Govindraj <krishnan@typedb.com>
2024-12-24 15:38:19 +0100

    Try without bazel sh?

--- .circleci/windows/prepare.bat
+++ .circleci/windows/prepare.bat
@@ -17,7 +17,7 @@ REM install runtime dependency for the build
 C:\Python37\python.exe -m pip install wheel
 
 REM permanently set variables for Bazel build
-SETX BAZEL_SH "C:\Program Files\Git\usr\bin\bash.exe"
+@REM SETX BAZEL_SH "C:\Program Files\Git\usr\bin\bash.exe"
 SETX BAZEL_PYTHON C:\Python37\python.exe
 @REM TODO: Update?
 @REM SETX BAZEL_VC "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC"
