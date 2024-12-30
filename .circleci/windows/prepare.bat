c119b0e11c4caf9cc79c7354cffa766d911742a4
Krishnan Govindraj <krishnan@typedb.com>
2024-12-30 14:48:11 +0530

    Try custom bazel python

--- .circleci/windows/prepare.bat
+++ .circleci/windows/prepare.bat
@@ -10,14 +10,15 @@ REM install dependencies needed for build
 choco install .circleci\windows\dependencies.config  --limit-output --yes --no-progress --allow-downgrade
 
 REM create a symlink python3.exe and make it available in %PATH%
-mklink C:\Python37\python3.exe C:\Python37\python.exe
-set PATH=%PATH%;C:\Python37
+mkdir python37
+curl -L https://www.python.org/ftp/python/3.7.8/python-3.7.8-embed-amd64.zip --output python37.zip
+tar -xf python37.zip -C python37
 
 REM install runtime dependency for the build
-C:\Python37\python.exe -m pip install wheel
+python37\python.exe -m pip install wheel
 
 REM permanently set variables for Bazel build
 @REM SETX BAZEL_SH "C:\Program Files\Git\usr\bin\bash.exe"
-SETX BAZEL_PYTHON C:\Python37\python.exe
+SETX BAZEL_PYTHON %cd%\python37\python3.exe
 @REM TODO: Update?
-SETX BAZEL_VC "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC"
+@REM SETX BAZEL_VC "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC"
