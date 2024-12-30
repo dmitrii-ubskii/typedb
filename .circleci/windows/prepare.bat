1af940a4ec32b9bcba57243ad24547cfe711dd07
Krishnan Govindraj <krishnan@typedb.com>
2024-12-30 22:34:00 +0530

    Does using bash as shell change anything

--- .circleci/windows/prepare.bat
+++ .circleci/windows/prepare.bat
@@ -19,5 +19,4 @@ C:\ProgramData\chocolatey\lib\python\python3.exe -m pip install wheel
 REM permanently set variables for Bazel build
 SETX BAZEL_SH "C:\Program Files\Git\usr\bin\bash.exe"
 SETX BAZEL_PYTHON "C:\ProgramData\chocolatey\lib\python\python.exe"
-@REM TODO: Update?
-@REM SETX BAZEL_VC "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC"
+SETX BAZEL_VC "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC"
