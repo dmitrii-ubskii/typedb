a1afbae2b774ed3e7bd873448ca71edd70a5f99a
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2025-02-27 16:23:59 +0000

    fix function selected variables

--- compiler/executable/match_/planner/mod.rs
+++ compiler/executable/match_/planner/mod.rs
@@ -243,7 +243,13 @@ impl StepBuilder {
                 assigned,
                 output_width,
                 ..
-            }) => ExecutionStep::FunctionCall(FunctionCallStep { function_id, arguments, assigned, output_width }),
+            }) => ExecutionStep::FunctionCall(FunctionCallStep {
+                function_id,
+                arguments,
+                assigned,
+                selected_variables,
+                output_width,
+            }),
         }
     }
 }
